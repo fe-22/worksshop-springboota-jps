8c5e6963d11f5ab711929387ad9aadec2a76b96b
Fernando Fernandes <fernandoalexthec@gmail.com>
2022-12-23 14:28:29 -0300

    Project created

---
+++ spfirst/src/main/java/com/fernandoalexthec/spfirst/SpfirstApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoalexthec.spfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpfirstApplication.class, args);
+	}
+
+}
---
+++ spfirst/src/test/java/com/fernandoalexthec/spfirst/SpfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoalexthec.spfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
